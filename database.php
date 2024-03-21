35550d0849c8de88dd25afa2250560d81506d77c
adamdfs <151184858+adamdfs@users.noreply.github.com>
2024-03-21 17:00:26 +0100

    Add files via upload

---
+++ database.php
@@ -0,0 +1,20 @@
+<?php
+function getDatabaseConnection() {
+// Paramètres de connexion à la base de données
+$host = 'localhost';
+$dbname = 'gestion_utilisateurs';
+$username = 'root';
+$password = ''; // XAMPP utilise par défaut pas de mot de passe
+
+
+try {
+    // Création d'une instance PDO pour la connexion à la base de données
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // Configuration des attributs PDO pour lancer une exception en cas d'erreur
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Gestion de l'erreur de connexion
+    die("Impossible de se connecter à la base de données: " . $e->getMessage());
+}
+}
+?>
